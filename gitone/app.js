a29eae684b9c7d9f61fb759af0c92b793d02a3e7
Dayan Qazi <dayanqazi622@gmail.com>
2024-03-06 04:31:25 +0500

    Work Done

---
+++ gitone/app.js
@@ -0,0 +1,109 @@
+// Q1,............................?
+
+
+// let player1 = prompt("Enter player name")
+// let player2 = prompt("Enter player name")
+
+// let toss = Math.random() * 2;
+
+// let change = Math.floor(toss)
+
+// if(change == 0){
+//     console.log(`${player1} You have Won the Toss and ${change}`);
+// }else{
+//     console.log(`${player2} You have Won the Toss and ${change}`);
+// }
+
+// ===================================================================================
+
+
+
+// Q2,............................?
+
+
+// let firstRamdan = new Date("2 march 2025")
+// let today = new Date()
+// let diff = firstRamdan.getTime() - today.getTime()
+
+// let days = diff / (1000 * 60 * 60 * 24)
+// let months = diff / (1000 * 60 * 60 * 24* 30)
+
+// console.log(Math.round(days));
+// console.log(Math.round(months));
+
+// ====================================================================================
+
+
+
+
+
+// Q3,............................?
+
+// let capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let small = "abcdefghijklmnopqrstuvwxyz";
+// let num = "1234567890";
+// let character = "!@$%^&*()#";
+
+// let randomcapital = "";
+// let randomsmall = "";
+// let randomnum = "";
+// let randomcharacter = "";
+
+
+// for (let i = 0; i <= 4; i++){
+
+//     let randomvalue = Math.floor( Math.random() * capital.length );
+//     randomcapital += capital[randomvalue];
+// };
+// for (let j = 0; j <= 4; j++){
+
+//     let randomvalue1 = Math.floor( Math.random() * small.length );
+//     randomsmall += small[randomvalue1];
+// };
+// for (let k = 0; k <= 4; k++){
+
+//     let randomvalue2 = Math.floor( Math.random() * num.length );
+//     randomnum += num[randomvalue2];
+// };
+// for (let l = 0; l <= 4; l++){
+
+//     let randomvalue3 = Math.floor( Math.random() * character.length );
+//     randomcharacter += character[randomvalue3];
+// };
+
+// let password = (randomcapital + randomsmall + randomnum + randomcharacter)
+
+
+
+// console.log(password);
+
+
+
+// ========================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
